aa429b0b01b71c503ba716b3d54b53876b041a76
enait <tainekta@gmail.com>
2026-06-10 14:49:55 +0100

    Added backend database config

---
+++ backend/app/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:postgres@localhost:5432/identity_detection",
+    )
+
+
+settings = Settings()
